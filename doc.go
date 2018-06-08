c4f86a336e00a3557e408dceed7cc7ab0754bacc
twold <twold52@gmail.com>
2018-06-08 11:22:39 -0400

    cleanup

--- doc.go
+++ doc.go
@@ -7,7 +7,8 @@
 //
 // Package API contains all supported operations. ServicerAPI interface methods
 // are named and organizated using a combination of the HTTP method and
-// the underlying endpoint. For example, "GET" requests to the [TIME_SERIES_DAILY_ADJUSTED](https://www.alphavantage.co/documentation/#dailyadj)
-// endpoint will be created using the GetTimeSeriesDaily method once the
-// Service receiver has been created using the desired input parameters.
+// the underlying endpoint. For example, "GET" requests to the TIME_SERIES_DAILY_ADJUSTED
+// api https://www.alphavantage.co/documentation/#dailyadj endpoint will be
+// created using the GetTimeSeriesDaily method once the *Service receiver
+// has been created using the desired input parameters.
 package galphaV
